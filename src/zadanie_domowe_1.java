996071f399fa2aa980af7a616eec1feb6b839502
Basia Kmiecik <basiakmiecik@gmail.com>
2018-09-09 19:19:59 +0200

    Pierwsza wersja plikow z zadania domowego

---
+++ src/zadanie_domowe_1.java
@@ -0,0 +1,7 @@
+public class zadanie_domowe_1 {
+    public static void main(String[] args) {
+        System.out.println("Uczę ");
+        System.out.println("się ");
+        System.out.println("programowania");
+    }
+}
